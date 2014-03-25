03b09cc2ef20604ca4b1b230e6f3a7bf390ff5dc
slogar25 <michaelslogar@gmail.com>
2014-03-24 20:03:11 -0400

    Revert "fix entropy errors"
    This reverts commit c86be4c7a6669ff980c596e5ba5fc113f74b911f.

--- include/linux/msm_vidc_enc.h
+++ include/linux/msm_vidc_enc.h
@@ -554,8 +554,7 @@ struct venc_capability{
 };
 
 struct venc_entropycfg{
-	unsigned long  	longentropysel;
-       // unsigned long   entropysel;
+	unsigned long	entropysel;
 	unsigned long	cabacmodel;
 };
 
